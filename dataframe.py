33da75fcbbd1046f22168c1d8cc0348a724f44ba
MICHAEL DUVAL <duv74@hotmail.com>
2019-08-15 14:35:46 -0400

    data commit

--- dataframe.py
+++ dataframe.py
@@ -4,6 +4,4 @@ raw_data = pd.read_csv('../web_project/final_report.csv')
 
 raw_data_df = pd.DataFrame(raw_data)
 
-print(raw_data_df).head()
-
 raw_data_df.to_html("../web_project/dataforsite.html")
